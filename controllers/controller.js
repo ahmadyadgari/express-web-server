22cb3646e83afd9497f0201738cce7bb39b94488
Your Name <you@example.com>
2025-01-25 02:21:35 -0800

    updated files =

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,47 @@
 import repo from './../db/techniquesRepo.js';
 
+const getPoints = (req, res) => {
+    const {x, y, z} = req.params;
+
+    if(!Number.isNaN(x) || !Number.isNaN(y) || !Number.isNaN(z)) {
+        res.status(400).send("Integers only pleae....");
+    } else {
+        res.status(200).send(`Point is: (${x}, ${y}, ${z})`);
+    }
+}
+
+const book = (req, res) => {
+    const thing = req.params.thing;
+    console.log(`Thing: ${thing}`);
+    
+    const book = {
+        name: "Andriods dream of electric sheep",
+        summary: "A bounty hunter that looks for andriod in present",
+        pages: 200
+        }
+        
+    if(thing in book) {
+        res.status(200).send(book[thing]);
+    } else {
+        res.status(404).send(`Book information not found`);
+    }
+}
+
+const bookName = (req, res) => {
+    res.status(200).send("Andriods dream of electric sheep");
+}
+
+const bookSummary = (req, res) => {
+    res.status(200).send("A bounty hunter that looks for andriod in present");
+}
+
+const bookPages = (req, res) => {
+    res.status(200).send("Pages: 200");
+}
+
+const author = (req, res) => {
+    res.status(200).send("Philip k Dick");
+}
 const printName = (req, res) => {
     const {fName, lName, nickname} = req.query;
     res.send(`Hello, ${fName} ${lName} ${nickname}!`);
@@ -57,5 +99,10 @@ export default {
     unknown,
     underwaterTechniques,
     pageLoader,
-    printName
+    printName, 
+    bookName,
+    bookSummary,
+    bookPages,
+    book,
+    getPoints
 }
